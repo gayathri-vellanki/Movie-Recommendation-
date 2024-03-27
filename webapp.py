213aef9373a54ca9aec95fb03485bc82204614ca
gayathri-vellanki <100945822+gayathri-vellanki@users.noreply.github.com>
2024-03-27 10:09:18 +0530

    apps

--- webapp.py
+++ webapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plts
+import matplotlib.pyplot as plt
 
 st.title("Movie Recommendation System")
 st.markdown('Using Collabrative Filtering')
